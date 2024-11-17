0178dddefff2770cd3beafc2a076b1039cf48b77
Mario564 <ka.mario564@gmail.com>
2024-11-17 13:17:01 -0800

    Update Redis init client

--- backend/src/singletons/redis.ts
+++ backend/src/singletons/redis.ts
@@ -1,4 +1,4 @@
 import Redis from 'ioredis';
 import { env } from '$src/utils/env';
 
-export const redis = new Redis(env.REDIS_URL);
+export const redis = new Redis(env.NODE_ENV === 'test' ? env.TEST_REDIS_URL : env.REDIS_URL);
--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -1,5 +1,6 @@
 import * as v from 'valibot';
 import * as s from '$src/utils/validation';
+import { validationError } from './error';
 
 export const Env = v.object({
   NODE_ENV: v.optional(
@@ -10,18 +11,18 @@ export const Env = v.object({
   DATABASE_URL: s.nonEmptyString(),
   TEST_DATABASE_URL: s.nonEmptyString(),
 
-  PUBLIC_OSU_CLIENT_ID: s.stringToInteger(),
+  PUBLIC_OSU_CLIENT_ID: s.nonEmptyString(),
   OSU_CLIENT_SECRET: s.nonEmptyString(),
   PUBLIC_OSU_REDIRECT_URI: v.pipe(v.string(), v.nonEmpty()),
 
-  PUBLIC_DISCORD_CLIENT_ID: s.stringToInteger(),
+  PUBLIC_DISCORD_CLIENT_ID: s.nonEmptyString(),
   DISCORD_CLIENT_SECRET: s.nonEmptyString(),
   PUBLIC_DISCORD_MAIN_REDIRECT_URI: s.nonEmptyString(),
   PUBLIC_DISCORD_CHANGE_ACCOUNT_REDIRECT_URI: s.nonEmptyString(),
   DISCORD_BOT_TOKEN: s.nonEmptyString(),
 
   REDIS_URL: s.nonEmptyString(),
-  REDIS_PASSWORD: s.nonEmptyString(),
+  TEST_REDIS_URL: s.nonEmptyString(),
 
   S3_FORCE_PATH_STYLE: s.stringToBoolean(),
   S3_ENDPOINT: s.nonEmptyString(),
@@ -36,4 +37,4 @@ export const Env = v.object({
 
 export type Env = v.InferOutput<typeof Env>;
 
-export const env = v.parse(Env, Bun.env);
+export const env = await v.parseAsync(Env, Bun.env).catch(validationError('Failed to parse env', 'env'));
