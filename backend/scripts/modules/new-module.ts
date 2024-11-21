e6818b1088dd16e8fab2755fee8fdad9b7eadefc
Mario564 <ka.mario564@gmail.com>
2024-11-21 15:27:23 -0800

    Add module creation script

---
+++ backend/scripts/modules/new-module.ts
@@ -0,0 +1,52 @@
+import { multiselect, text } from '@clack/prompts';
+import { mkdirSync, readdirSync } from 'node:fs';
+import { writeRepositoryFile, writeServiceFile, writeValidationFile } from './write-file';
+
+const folderName = await text({
+  message: 'Module folder name',
+  placeholder: 'my-module',
+  validate(value) {
+    if (value.length === 0) return 'Specify a folder name';
+  }
+});
+
+if (typeof folderName === 'symbol') {
+  process.exit(0);
+}
+
+const folderNames = readdirSync(`${process.cwd()}/src/modules`, {
+  withFileTypes: true
+}).filter((item) => item.isDirectory()).map((folder) => folder.name);
+
+if (folderNames.includes(folderName)) {
+  console.log(`Folder ${folderName} already exists`);
+  process.exit(1);
+}
+
+const files = await multiselect({
+  message: 'Files to create',
+  options: [
+    { value: 'repository.ts' },
+    { value: 'service.ts' },
+    { value: 'validation.ts' }
+  ],
+  required: true
+});
+
+if (typeof files === 'symbol') {
+  process.exit(0);
+}
+
+mkdirSync(`${process.cwd()}/src/modules/${folderName}`);
+
+for (const file of files) {
+  if (file === 'repository.ts') {
+    await writeRepositoryFile(folderName);
+  } else if (file === 'service.ts') {
+    await writeServiceFile(folderName);
+  } else {
+    await writeValidationFile(folderName);
+  }
+}
+
+process.exit(0);
---
+++ backend/scripts/modules/write-file.ts
@@ -0,0 +1,40 @@
+import { writeFileSync } from 'node:fs';
+import { camelCase, pascalCase } from 'scule';
+
+export async function writeRepositoryFile(folder: string) {
+  writeFileSync(
+    `${process.cwd()}/src/modules/${folder}/repository.ts`,
+    await Bun
+      .file(`${process.cwd()}/scripts/modules/repository.template.txt`)
+      .text()
+      .then((txt) => txt.replace('{$1}', camelCase(folder)))
+  );
+}
+
+export async function writeServiceFile(folder: string) {
+  writeFileSync(
+    `${process.cwd()}/src/modules/${folder}/service.ts`,
+    await Bun
+      .file(`${process.cwd()}/scripts/modules/service.template.txt`)
+      .text()
+      .then(
+        (txt) => txt
+          .replace('{$1}', camelCase(folder))
+          .replace('{$2}', pascalCase(folder))
+          .replace('{$3}', camelCase(folder))
+      )
+  );
+}
+
+export async function writeValidationFile(folder: string) {
+  writeFileSync(
+    `${process.cwd()}/src/modules/${folder}/validation.ts`,
+    await Bun
+      .file(`${process.cwd()}/scripts/modules/validation.template.txt`)
+      .text()
+      .then(
+        (txt) => txt
+          .replace('{$1}', pascalCase(folder))
+      )
+  );
+}
