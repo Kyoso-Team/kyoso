97eb493f8287159170f4d86eada0050d05c8f514
Mario564 <ka.mario564@gmail.com>
2025-01-06 11:16:14 -0800

    Some notification related stuff

---
+++ backend/src/modules/notifications/service.ts
@@ -0,0 +1,6 @@
+import * as v from 'valibot';
+import { notificationsRepository } from './repository';
+import { NotificationsValidation } from './validation';
+
+
+export const notificationsService = {  };
