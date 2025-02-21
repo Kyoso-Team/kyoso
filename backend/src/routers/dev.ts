62b1e300ba7fdff28fc43568b074e0715d870493
ArtemOsuskyi <a.osuskiy@gmail.com>
2025-02-21 17:24:16 +0200

    add dev env validation for compatibility with hono client

--- backend/src/routers/dev.ts
+++ backend/src/routers/dev.ts
@@ -2,12 +2,14 @@ import { vValidator } from '@hono/valibot-validator';
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
 import * as v from 'valibot';
+import { devCheckMiddleware } from '$src/middlewares/dev.ts';
 import { sessionMiddleware } from '$src/middlewares/session.ts';
 import { devService } from '$src/modules/dev/service.ts';
 import { integerId } from '$src/utils/validation.ts';
 
 export const devRouter = new Hono()
   .basePath('/dev')
+  .use(devCheckMiddleware)
   .put(
     'impersonate',
     vValidator(
